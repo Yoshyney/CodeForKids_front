48a5f4517c8762ed4f023f8309877f8ad961677e
CorentinHOU <CorentinHOU@users.noreply.github.com>
2021-10-06 00:16:38 +0200

    création du plateau de code + tirroire d'édition

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Board from "./component/Board" ;
+import Card from "./component/Card" ;
+import Drawer from "./component/Drawer" ;
 
 function App() {
+  const [state, setstate] = useState(false);
+
+  const toggler = () => {
+    state ? setstate(false): setstate(true);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main className="flexbox">
+
+        <Board id="board" className="board">
+        </Board>
+
+        <button id="button-drawer" className="button-drawer" onClick={toggler}>Edit</button>
+        {state ? 
+          <Drawer id="drawer" className="drawer">
+            <Card id="card-1" className="card" draggable="true">
+              <p>MOUV FORWARD</p>
+            </Card>
+            <Card id="card-2" className="card" draggable="true">
+              <p>WHILE</p>
+            </Card>
+            <Card id="card-3" className="card" draggable="true">
+              <p>END WHILE</p>
+            </Card>
+          </Drawer> 
+          : null
+        }
+
+      </main>
     </div>
   );
 }
